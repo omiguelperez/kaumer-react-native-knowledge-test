b609e0df8ed3a16eeb97e105735086980351300a
Oscar Pérez <mr.omiguelperez@gmail.com>
2018-11-15 09:02:09 -0500

    Left drawer with react-navigation

--- App.js
+++ App.js
@@ -1,48 +1,56 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+import { createDrawerNavigator, DrawerNavigator } from 'react-navigation';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+class MyHomeScreen extends React.Component {
+  static navigationOptions = {
+    drawerLabel: 'Home',
+  };
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View>
+        <Text>Home page!</Text>
+        <Button
+          onPress={() => this.props.navigation.toggleDrawer()}
+          title="DrawerOpen"
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+class MyNotificationsScreen extends React.Component {
+  static navigationOptions = {
+    drawerLabel: 'Notifications',
+  };
+
+  render() {
+    return (
+      <View>
+        <Button
+          onPress={() => this.props.navigation.goBack()}
+          title="Go back home"
+        />
+        <Button
+          onPress={() => this.props.navigation.toggleDrawer()}
+          title="DrawerOpen"
+        />
+      </View>
+    );
+  }
+}
+
+
+export default DrawerNavigator({
+  Home: {
+    screen: MyHomeScreen,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  Notifications: {
+    screen: MyNotificationsScreen,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+}, {
+  drawerPosition: 'left',
+  initialRouteName: 'Home',
+  drawerWidth: 200
 });

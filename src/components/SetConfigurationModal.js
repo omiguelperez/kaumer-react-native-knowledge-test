afd35914bfbd5a465ae2be6695c0d597809161a1
Oscar Pérez <mr.omiguelperez@gmail.com>
2018-11-16 20:15:01 -0500

    Add react-native-vector-icons FontAwesome, confuring form

--- src/components/SetConfigurationModal.js
+++ src/components/SetConfigurationModal.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import { Modal, StyleSheet, Text, TouchableHighlight, View, Alert } from 'react-native';
 import ModalHeader from '../components/ModalHeader';
-import { Form, Item, Label, Input, Content, Container, Icon } from 'native-base'
+import { Form, Item, Label, Input, Content, Container } from 'native-base'
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class SetConfigurationModal extends Component {
+  state = {
+    year: new Date().getFullYear()
+  }
+
   render() {
     return (
       <Modal
@@ -14,14 +19,146 @@ export default class SetConfigurationModal extends Component {
           Alert.alert('Modal has been closed.');
         }}>
         <Content>
-        <ModalHeader
+          <ModalHeader
             closeModal={() => this.props.setModalVisible(false)}
             title="Configurando..."
             saveOption={true}
             saveOptionCallback={this.saveEmployee}
           />
+          <View style={styles.yearContainer}>
+            <Text style={styles.yearText}>{ this.state.year }</Text>
+          </View>
+          <Form style={styles.form}>
+            <Item style={[styles.formInput, styles.staticInput]}>
+              <Icon
+                style={styles.icon}
+                name="ios-cash"
+                size={30}
+                color="#000" />
+              <Input
+                keyboardType="numeric"
+                placeholder="Salario mínimo" />
+            </Item>
+            <Item style={[styles.formInput, styles.staticInput]}>
+              <Icon
+                style={styles.icon}
+                name="ios-cash"
+                size={30}
+                color="#000" />
+              <Input
+                keyboardType="numeric"
+                placeholder="Auxliio de transporte" />
+            </Item>
+            <Item style={[styles.formInput, styles.staticInput]}>
+              <Icon
+                style={[styles.icon, styles.percentageIcon]}
+                name="ios-calculator"
+                size={30}
+                color="#000" />
+              <Input
+                keyboardType="numeric"
+                placeholder="Vacaciones" />
+            </Item>
+            <Item style={[styles.formInput, styles.staticInput]}>
+              <Icon
+                style={[styles.icon, styles.percentageIcon]}
+                name="ios-calculator"
+                size={30}
+                color="#000" />
+              <Input
+                keyboardType="numeric"
+                placeholder="Cesantias" />
+            </Item>
+            <Item style={[styles.formInput, styles.staticInput]}>
+              <Icon
+                style={[styles.icon, styles.percentageIcon]}
+                name="ios-calculator"
+                size={30}
+                color="#000" />
+              <Input
+                keyboardType="numeric"
+                placeholder="Intereses de Cesantias" />
+            </Item>
+            <Item style={[styles.formInput, styles.staticInput]}>
+              <Icon
+                style={[styles.icon, styles.percentageIcon]}
+                name="ios-calculator"
+                size={30}
+                color="#000" />
+              <Input
+                keyboardType="numeric"
+                placeholder="Prima de servicios" />
+            </Item>
+            <Item style={[styles.formInput, styles.staticInput]}>
+              <Icon
+                style={[styles.icon, styles.percentageIcon]}
+                name="ios-calculator"
+                size={30}
+                color="#000" />
+              <Input
+                keyboardType="numeric"
+                placeholder="Salud" />
+            </Item>
+            <Item style={[styles.formInput, styles.staticInput]}>
+              <Icon
+                style={[styles.icon, styles.percentageIcon]}
+                name="ios-calculator"
+                size={30}
+                color="#000" />
+              <Input
+                keyboardType="numeric"
+                placeholder="Pensión" />
+            </Item>
+            <Item style={[styles.formInput, styles.staticInput]}>
+              <Icon
+                style={[styles.icon, styles.percentageIcon]}
+                name="ios-calculator"
+                size={30}
+                color="#000" />
+              <Input
+                keyboardType="numeric"
+                placeholder="Riesgos Profesionales (Clase I)" />
+            </Item>
+            <Item style={[styles.formInput, styles.staticInput]}>
+              <Icon
+                style={[styles.icon, styles.percentageIcon]}
+                name="ios-calculator"
+                size={30}
+                color="#000" />
+              <Input
+                keyboardType="numeric"
+                placeholder="Aportes (Caja)" />
+            </Item>
+          </Form>
         </Content>
       </Modal>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  form: {
+    paddingHorizontal: 20,
+    marginBottom: 40,
+  },
+  formInput: {
+    marginLeft: 0,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  percentageIcon: {
+    marginHorizontal: 10,
+  },
+  staticInput: {
+    marginTop: 18,
+  },
+  yearContainer: {
+    paddingVertical: 10,
+  },
+  yearText: {
+    alignSelf: 'center',
+    fontSize: 20,
+    color: 'gray',
+  }
+})
